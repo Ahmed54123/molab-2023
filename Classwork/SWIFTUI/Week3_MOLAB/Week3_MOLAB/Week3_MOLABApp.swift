c985762fc5e6b07b4ca135679a82273a89baa709
Ahmed54123 <ad5972@nyu.edu>
2023-10-05 09:26:41 -0400

    DateTime added
    added wk4 assignment

---
+++ Classwork/SWIFTUI/Week3_MOLAB/Week3_MOLAB/Week3_MOLABApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week3_MOLABApp.swift
+//  Week3_MOLAB
+//
+//  Created by ahmed casual on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Week3_MOLABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
